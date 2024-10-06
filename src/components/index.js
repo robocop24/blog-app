04c5f87e1d7e2bf0c09b9279fe5020178e83fedb
robocop24 <suhailriyaz94@gmail.com>
2024-10-06 20:06:57 +0530

    login form wip

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,15 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn"
+import Login from "./Login";
+import Button from "./Button";
 
 export {
     Header,
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    Login,
+    Button,
 }
